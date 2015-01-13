a81a0fed3e208246b0cb139c172243bb32bf1eca
KyleCRat <KyleCRat@me.com>
2015-01-13 00:00:55 -0500

    routed '/' to views/index.html

--- app.rb
+++ app.rb
@@ -3,16 +3,7 @@ require 'sinatra'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    "
-    <!DOCTYPE html><html lang='en'><head><meta charset='UTF-8' />
-    <title>Kyle's First app.rb</title>
-    <link rel='stylesheet' href='/style.css' />
-    </head>
-    <body>
-    <header>Kyle Ratliff's first app.rb</header>
-    	<div style='background-color: #888888;' >Hello World!</div>
-    </body>
-    </html>
-	"
+    send_file 'views/index.html'
+    send_file 'css/style.css'
   end
 end
